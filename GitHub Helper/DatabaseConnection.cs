fe43394565cc4bd1f2bcdc51c2a6a5efedc3d67e
Mateusz Przydacz <reliableassassin@gmail.com>
2024-05-10 18:08:31 +0200

    Dodanie klasy służącej do połaczenia SQLite

---
+++ GitHub Helper/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Helper
+{
+    public class DatabaseConnection
+    {
+        private string _connectionString;
+
+        public DatabaseConnection(string databaseName)
+        {
+            _connectionString = $"Data Source={databaseName}.db";
+        }
+
+        public SqliteConnection GetConnection()
+        {
+            return new SqliteConnection(_connectionString);
+        }
+    }
+}
--- GitHub Helper/Program.cs
+++ GitHub Helper/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using GitHub_Helper;
+
+var dbConnection = new DatabaseConnection("GitHub_Helper");
+using (var connection = dbConnection.GetConnection())
+{
+    connection.Open();
+}
+
+Console.ReadLine();
